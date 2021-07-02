5f4320bafbdcb0b6826b5ae4293def2bd3a1bb12
TanyaFreebie <escurio@gmail.com>
2021-07-02 19:21:15 +0300

    next lesson

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -12,20 +14,63 @@ public class Main {
     */
 
     //1.step Start a loop
-        for(int i = 1; i <= 50; i++){
+       /* for(int i = 1; i <= 50; i++){
             //2.step start if
 
                if (i % 3 == 0 && i % 7 == 0) { //FizBuzz case will go first
                     System.out.println("FizzBuzz");
+                    continue;
                 } else if (i %3 == 0) { //Fizz case
                     System.out.println("Fizz");
+                    continue;
                 } else if (i % 7 ==0) { //Buzz case
                     System.out.println("Buzz");
-                } else {
-                    System.out.println(i);
+                    continue;
                 }
-
+            System.out.println(i);
             }
+            */
+
+        //collect input
+        //solve discriminant
+        //find first x
+        //find second x
+        //output result
+
+        //Get user input from console
+        //initialize scanner
+        Scanner scanner = new Scanner(System.in);
+
+        //get values of a
+        System.out.print("Enter the value of a: ");
+        double a = scanner.nextDouble();
+        //get values of b
+        System.out.print("Enter the value of b: ");
+        double b = scanner.nextDouble();
+        //get values of c
+        System.out.print("Enter the value of c: ");
+        double c = scanner.nextDouble();
+
+        //solve for the discriminant -(b^2 -4 * a *c) PEMDAS-
+        double discriminant = Math.pow(b, 2) -(4 * a * c);
+
+        //if discriminant is negative then exit the program
+        if (discriminant < 0){
+            System.out.println("Discriminant is negative");
+            return;
+        }
+
+        //first value of x
+        //(-b + sqrt(discr))/2 *a
+        double x1 = (-b + Math.sqrt(discriminant))/ 2 * a;
+
+        // solve for second value of x
+        //(-b - sqrt(discr))/2 *a
+        double x2 = (-b - Math.sqrt(discriminant))/ 2 * a;
+
+        //output
+        System.out.println("The first value of x: " + x1);
+        System.out.println("The second value of x: " + x2);
         }
     }
 
